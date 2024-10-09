b6f00739b109a2762e6c6ec3adb3cd39dcc8a07c
Naahiki <112497828+Naahiki@users.noreply.github.com>
2024-10-09 11:15:19 +0200

    Railway-conf4

--- import-db.js
+++ import-db.js
@@ -7,12 +7,13 @@ dotenv.config();
 
 // Crear conexión a la base de datos
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT, // Usar el puerto proporcionado por Railway
+    decimalNumbers: true,
+  });
 
 db.connect((err) => {
   if (err) {
