672ac0736bbf87df44c6553e80cbffc5c1c8a1bd
Naahiki <112497828+Naahiki@users.noreply.github.com>
2024-10-09 20:07:15 +0200

    Railway-conf18-no sql2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
-
+const fs = require('fs');
 
 // Inicializar dotenv para usar variables de entorno desde el archivo .env
 dotenv.config();
@@ -54,55 +54,115 @@ db.connect((err) => {
   }
   console.log('Conectado a MySQL');
 
-  // Aquí ejecutas las consultas directamente
-  const createUsersTableQuery = `
-    DROP TABLE IF EXISTS users;
-    CREATE TABLE users (
-      id_users INT NOT NULL,
-      user_first_name VARCHAR(45) NOT NULL,
-      user_email VARCHAR(225) NOT NULL,
-      user_last_name VARCHAR(45) NOT NULL,
-      user_password VARCHAR(225) NOT NULL,
-      user_status TINYINT(1) DEFAULT '0',
-      PRIMARY KEY (id_users),
-      UNIQUE KEY (user_email)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-  `;
+  // Solo importar la base de datos si se especifica en las variables de entorno
+  if (process.env.RUN_IMPORT === 'true') {
+    console.log("Ejecutando la importación de la base de datos...");
 
-  const createOrdersTableQuery = `
-    DROP TABLE IF EXISTS orders;
-    CREATE TABLE orders (
-      order_id INT NOT NULL AUTO_INCREMENT,
-      id_users INT DEFAULT NULL,
-      address VARCHAR(255) DEFAULT NULL,
-      city VARCHAR(100) DEFAULT NULL,
-      postal_code VARCHAR(10) DEFAULT NULL,
-      payment_method VARCHAR(50) NOT NULL,
-      status TINYINT(1) DEFAULT NULL,
-      order_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-      PRIMARY KEY (order_id),
-      KEY id_users (id_users),
-      CONSTRAINT orders_ibfk_1 FOREIGN KEY (id_users) REFERENCES users (id_users)
-    ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-  `;
+    // Definición de las consultas
+    const queries = [
+      // Consultas para la tabla `users`
+      `DROP TABLE IF EXISTS \`users\`;`,
+      `CREATE TABLE \`users\` (
+        \`id_users\` INT NOT NULL,
+        \`user_first_name\` VARCHAR(45) NOT NULL,
+        \`user_email\` VARCHAR(225) NOT NULL,
+        \`user_last_name\` VARCHAR(45) NOT NULL,
+        \`user_password\` VARCHAR(225) NOT NULL,
+        \`user_status\` TINYINT(1) DEFAULT '0',
+        PRIMARY KEY (\`id_users\`),
+        UNIQUE KEY (\`user_email\`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`,
 
-  // Ejecutar las consultas en serie
-  db.query(createUsersTableQuery, (err, result) => {
-    if (err) {
-      console.error('Error creando la tabla users:', err);
-    } else {
-      console.log('Tabla users creada con éxito.');
-      // Ejecuta la siguiente consulta
-      db.query(createOrdersTableQuery, (err, result) => {
-        if (err) {
-          console.error('Error creando la tabla orders:', err);
-        } else {
-          console.log('Tabla orders creada con éxito.');
-        }
+      // Consultas para la tabla `travel_packs`
+      `DROP TABLE IF EXISTS \`travel_packs\`;`,
+      `CREATE TABLE \`travel_packs\` (
+        \`pack_id\` INT NOT NULL,
+        \`pack_title\` VARCHAR(225) NOT NULL,
+        \`pack_destination\` VARCHAR(225) NOT NULL,
+        \`pack_category\` VARCHAR(45) NOT NULL,
+        \`pack_price\` DECIMAL(10,2) NOT NULL,
+        \`pack_date\` VARCHAR(225) NOT NULL,
+        \`pack_image\` VARCHAR(225) NOT NULL,
+        \`pack_amount\` INT NOT NULL,
+        PRIMARY KEY (\`pack_id\`),
+        UNIQUE KEY (\`pack_id\`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`,
+
+      // Insertar datos en la tabla `travel_packs`
+      `INSERT INTO \`travel_packs\` (\`pack_id\`, \`pack_title\`, \`pack_destination\`, \`pack_category\`, \`pack_price\`, \`pack_date\`, \`pack_image\`, \`pack_amount\`) VALUES 
+      (1, 'Aventura en los Alpes', 'Suiza', 'Aventura', 1500.00, 'Julio', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1727947904/Alpes_e4jby3.jpg', 10),
+      (2, 'Aventura en Turquía con Globos Aerostáticos', 'Turquía', 'Aventura', 1800.00, 'Agosto', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1727947037/samples/balloons.jpg', 5),
+      (3, 'Naturaleza Exuberante', 'Montañas', 'Relax', 1200.00, 'Septiembre', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1727947031/samples/landscapes/nature-mountains.jpg', 8),
+      (4, 'Escapada de Playa', 'Islas Maldivas', 'Familiar', 2200.00, 'Diciembre', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1727947031/samples/landscapes/beach-boat.jpg', 2),
+      (5, 'Aventura en la Montaña Mágica', 'Andes', 'Aventura', 1300.00, 'Octubre', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1727947039/cld-sample-2.jpg', 6),
+      (6, 'Tour Gastronómico por Italia', 'Italia', 'Gastronomía', 2000.00, 'Noviembre', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1727947040/cld-sample-4.jpg', 4),
+      (7, 'Descubre el Oriente Místico', 'Japón', 'Cultural', 2500.00, 'Marzo', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1727947031/samples/food/spices.jpg', 7),
+      (8, 'Explorando Nueva York', 'Nueva York', 'Cultural', 3000.00, 'Abril', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1727947031/samples/landscapes/architecture-signs.jpg', 0),
+      (9, 'Escapada Rural a la Campiña Inglesa', 'Inglaterra', 'Rural', 1100.00, 'Junio', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1727947030/samples/sheep.jpg', 9),
+      (10, 'Romántico París', 'Francia', 'Cultural', 600.00, 'Mayo', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1728246145/Paris_nt1qjw.jpg', 12),
+      (11, 'Exploración de Ámsterdam en Bicicleta', 'Países Bajos', 'Aventura', 450.00, 'Junio', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1728246202/amster_w6dowu.jpg', 9),
+      (12, 'Tradiciones de Japón', 'Japón', 'Cultural', 900.00, 'Octubre', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1728246235/japon_gj2td8.jpg', 6),
+      (13, 'Café y Cultura en Colombia', 'Colombia', 'Cultural', 750.00, 'Noviembre', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1728246234/colombia_ulhwhk.jpg', 5),
+      (14, 'Paraíso Cubano en Varadero', 'Cuba', 'Relax', 500.00, 'Agosto', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1728246270/cua_hk2egi.jpg', 7),
+      (15, 'Aventura en Euskal Herria', 'Euskal Herria', 'Aventura', 650.00, 'Julio', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1728284543/euskadi_ttqcxr.jpg', 8),
+      (16, 'Vibrante Barcelona', 'España', 'Cultural', 550.00, 'Septiembre', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1728284543/bcn_c3ckal.png', 10),
+      (17, 'Relax en las Playas de Miami', 'Estados Unidos', 'Relax', 800.00, 'Diciembre', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1728284632/MIAMI_ydqlgy.jpg', 6),
+      (18, 'Escapada Escocesa', 'Reino Unido', 'Cultural', 700.00, 'Abril', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1728284714/escocia_gub4t0.jpg', 0),
+      (19, 'Rutas Verdes de Irlanda', 'Irlanda', 'Aventura', 750.00, 'Marzo', 'https://res.cloudinary.com/diej1zlz4/image/upload/v1728284714/irlanda_gd6xsp.jpg', 5);`,
+
+      // Eliminar la tabla `orders` si ya existe
+      `DROP TABLE IF EXISTS \`orders\`;`,
+      
+      // Crear la tabla `orders`
+      `CREATE TABLE \`orders\` (
+        \`order_id\` INT NOT NULL AUTO_INCREMENT,
+        \`id_users\` INT DEFAULT NULL,
+        \`address\` VARCHAR(255) DEFAULT NULL,
+        \`city\` VARCHAR(100) DEFAULT NULL,
+        \`postal_code\` VARCHAR(10) DEFAULT NULL,
+        \`payment_method\` VARCHAR(50) NOT NULL,
+        \`status\` TINYINT(1) DEFAULT NULL,
+        \`order_date\` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (\`order_id\`),
+        KEY (\`id_users\`),
+        CONSTRAINT \`orders_ibfk_1\` FOREIGN KEY (\`id_users\`) REFERENCES \`users\` (\`id_users\`)
+      ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`,
+
+      // Eliminar la tabla `order_items` si ya existe
+      `DROP TABLE IF EXISTS \`order_items\`;`,
+      
+      // Crear la tabla `order_items`
+      `CREATE TABLE \`order_items\` (
+        \`order_item_id\` INT NOT NULL AUTO_INCREMENT,
+        \`order_id\` INT DEFAULT NULL,
+        \`pack_id\` INT DEFAULT NULL,
+        \`quantity\` INT DEFAULT NULL,
+        PRIMARY KEY (\`order_item_id\`),
+        KEY (\`order_id\`),
+        KEY (\`pack_id\`),
+        CONSTRAINT \`order_items_ibfk_1\` FOREIGN KEY (\`order_id\`) REFERENCES \`orders\` (\`order_id\`) ON DELETE CASCADE,
+        CONSTRAINT \`order_items_ibfk_2\` FOREIGN KEY (\`pack_id\`) REFERENCES \`travel_packs\` (\`pack_id\`) ON DELETE CASCADE
+      ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`
+    ];
+
+    // Función para ejecutar las consultas secuencialmente
+    const executeQueries = (queries) => {
+      queries.forEach((query) => {
+        db.query(query, (err, result) => {
+          if (err) {
+            console.error('Error ejecutando la consulta:', err);
+            return;
+          }
+          console.log('Consulta ejecutada con éxito:', query);
+        });
       });
-    }
-  });
+    };
+
+    // Ejecutar las consultas
+    executeQueries(queries);
+  }
 });
+
 
 // Ruta de ejemplo
 app.get('/', (req, res) => {
