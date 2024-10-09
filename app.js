39db9b3b1f415095fe7ab12df0043b24a239bba5
Naahiki <112497828+Naahiki@users.noreply.github.com>
2024-10-09 20:24:38 +0200

    Railway-conf18-no sql9

--- app.js
+++ app.js
@@ -61,16 +61,16 @@ db.connect((err) => {
     // Definición de las consultas
     const queries = [
       // Consultas para la tabla `users`
-      `DROP TABLE IF EXISTS \`users\`;`,
-      `CREATE TABLE \`users\` (
-        \`id_users\` INT NOT NULL,
-        \`user_first_name\` VARCHAR(45) NOT NULL,
-        \`user_email\` VARCHAR(225) NOT NULL,
-        \`user_last_name\` VARCHAR(45) NOT NULL,
-        \`user_password\` VARCHAR(225) NOT NULL,
-        \`user_status\` TINYINT(1) DEFAULT '0',
-        PRIMARY KEY (\`id_users\`),
-        UNIQUE KEY (\`user_email\`)
+      //`DROP TABLE IF EXISTS users;`,
+      `CREATE TABLE users (
+        id_users INT NOT NULL AUTO_INCREMENT,
+        user_first_name VARCHAR(45) NOT NULL,
+        user_email VARCHAR(225) NOT NULL,
+        user_last_name VARCHAR(45) NOT NULL,
+        user_password VARCHAR(225) NOT NULL,
+        user_status TINYINT(1) DEFAULT '0',
+        PRIMARY KEY (id_users),
+        UNIQUE KEY (user_email)
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;`,
 
       // Consultas para la tabla `travel_packs`
