bf7bfe82ae35e2f2ca19f31d383f5369c7b2ee89
Naahiki <112497828+Naahiki@users.noreply.github.com>
2024-10-09 18:48:47 +0200

    Railway-conf18-all2

--- db/travel_ecommerce_users.sql
+++ db/travel_ecommerce_users.sql
@@ -3,12 +3,13 @@ DROP TABLE IF EXISTS users;
 
 -- Crear la tabla `users`
 CREATE TABLE users (
-  id_users INT NOT NULL AUTO_INCREMENT,
+  id_users INT NOT NULL,  
   user_first_name VARCHAR(45) NOT NULL,
   user_email VARCHAR(225) NOT NULL,
   user_last_name VARCHAR(45) NOT NULL,
   user_password VARCHAR(225) NOT NULL,
   user_status TINYINT(1) DEFAULT '0',
   PRIMARY KEY (id_users),
-  UNIQUE KEY (user_email) 
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY (user_email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci; 
+
